050136d7223806dcf613346f46a0c322c15a81b1
David Guevara <david.guevara4137@alumnos.udg.mx>
2020-10-19 19:29:55 -0500

    Burbuja.go

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,25 @@
 package main
 
-func Burbuja(s []int64)  {
+import "fmt"
 
+func Burbuja(s []int64) {
+	//aux := 0
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[j] < s[i] {
+				aux := s[i]
+				s[i] = s[j]
+				s[j] = aux
+			}
+		}
+	}
+	fmt.Println(s)
 }
 
-func main()  {
-	
+func main() {
+
+	//s := []int64{1, 6, 2, 4, 7, 0, -2, -1, 5, 3}
+	fmt.Println(s)
+	Burbuja(s)
+
 }
